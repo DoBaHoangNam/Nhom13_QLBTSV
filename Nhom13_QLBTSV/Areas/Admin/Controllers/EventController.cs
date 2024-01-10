745ab0bc52ae19ce557d4fd792764e3959325771
Hoang Nam <hoangnam2412lc@gmail.com>
2024-01-10 22:34:16 +0700

    upload base project

---
+++ Nhom13_QLBTSV/Areas/Admin/Controllers/EventController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBTSV.Areas.Admin.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Admin/Event
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
